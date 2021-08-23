275e270aaa0be8c4df0c8edbde0ebc8be5ee439c
luiselmuller <luiselmuller@gmail.com>
2021-08-23 14:27:35 -0400

    Changed stuff, added other stuff
    Added media queries, added a js file but nothing works in it for now, changed the scrollbar style just because

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+const learnMore = document.getElementById('learnmore-btn');
+const droidBtn = document.getElementById('droid2d2-btn');
+
+learnMore.onclick = () => {
+    alert("ERROR: Block2d2 not found, please contact an admin.");
+}
+
+droidBtn.onclick = () => {
+    alert("ERROR: Droid2d2 not found, please contact an admin.");
+}
+
+/* WILL FIX SOMEDAY */
